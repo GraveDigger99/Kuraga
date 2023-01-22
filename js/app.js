3858d4cadb701e5a15cd7bf80ed9656de223e229
Ibrahimov Aydin <80528765+GraveDigger99@users.noreply.github.com>
2023-01-23 01:12:30 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -118,6 +118,76 @@
         }
     }
     modules_flsModules.watcher = new ScrollWatcher({});
+    class Parallax {
+        constructor(elements) {
+            if (elements.length) this.elements = Array.from(elements).map((el => new Parallax.Each(el, this.options)));
+        }
+        destroyEvents() {
+            this.elements.forEach((el => {
+                el.destroyEvents();
+            }));
+        }
+        setEvents() {
+            this.elements.forEach((el => {
+                el.setEvents();
+            }));
+        }
+    }
+    Parallax.Each = class {
+        constructor(parent) {
+            this.parent = parent;
+            this.elements = this.parent.querySelectorAll("[data-prlx]");
+            this.animation = this.animationFrame.bind(this);
+            this.offset = 0;
+            this.value = 0;
+            this.smooth = parent.dataset.smooth ? Number(parent.dataset.smooth) : 15;
+            this.setEvents();
+        }
+        setEvents() {
+            this.animationID = window.requestAnimationFrame(this.animation);
+        }
+        destroyEvents() {
+            window.cancelAnimationFrame(this.animationID);
+        }
+        animationFrame() {
+            const topToWindow = this.parent.getBoundingClientRect().top;
+            const heightParent = this.parent.offsetHeight;
+            const heightWindow = window.innerHeight;
+            const positionParent = {
+                top: topToWindow - heightWindow,
+                bottom: topToWindow + heightParent
+            };
+            const centerPoint = this.parent.dataset.center ? this.parent.dataset.center : "center";
+            if (positionParent.top < 30 && positionParent.bottom > -30) switch (centerPoint) {
+              case "top":
+                this.offset = -1 * topToWindow;
+                break;
+
+              case "center":
+                this.offset = heightWindow / 2 - (topToWindow + heightParent / 2);
+                break;
+
+              case "bottom":
+                this.offset = heightWindow - (topToWindow + heightParent);
+                break;
+            }
+            this.value += (this.offset - this.value) / this.smooth;
+            this.animationID = window.requestAnimationFrame(this.animation);
+            this.elements.forEach((el => {
+                const parameters = {
+                    axis: el.dataset.axis ? el.dataset.axis : "v",
+                    direction: el.dataset.direction ? el.dataset.direction + "1" : "-1",
+                    coefficient: el.dataset.coefficient ? Number(el.dataset.coefficient) : 5,
+                    additionalProperties: el.dataset.properties ? el.dataset.properties : ""
+                };
+                this.parameters(el, parameters);
+            }));
+        }
+        parameters(el, parameters) {
+            if ("v" == parameters.axis) el.style.transform = `translate3D(0, ${(parameters.direction * (this.value / parameters.coefficient)).toFixed(2)}px,0) ${parameters.additionalProperties}`; else if ("h" == parameters.axis) el.style.transform = `translate3D(${(parameters.direction * (this.value / parameters.coefficient)).toFixed(2)}px,0,0) ${parameters.additionalProperties}`;
+        }
+    };
+    if (document.querySelectorAll("[data-prlx-parent]")) modules_flsModules.parallax = new Parallax(document.querySelectorAll("[data-prlx-parent]"));
     let addWindowScrollEvent = false;
     setTimeout((() => {
         if (addWindowScrollEvent) {
@@ -127,10 +197,11 @@
             }));
         }
     }), 0);
-    let scrollSpeed = 100;
+    let scrollSpeed = 300;
     let scroller = document.getElementById("scroll");
     const heroMove = document.querySelector(".stages__hero");
-    scroller.addEventListener("wheel", (e => {
+    scroller.addEventListener("wheel", (function(e) {
+        setTimeout((function() {}), 200);
         let scrollCountHero = 1.23 * scroller.scrollLeft + "px";
         heroMove.style.left = scrollCountHero;
         if (heroMove.style.left >= 5200 + "px") heroMove.style.left = 0 + "px"; else heroMove.style.left = scrollCountHero;
@@ -138,7 +209,7 @@
         if (scrollCount > 0 + "px") document.documentElement.classList.add("_scroll"); else document.documentElement.classList.remove("_scroll");
         if (!e.deltaY) return;
         let scrollDirection = e.deltaY > 0 ? 1 : -1;
-        scroller.scrollLeft += scrollSpeed * scrollDirection;
+        scroller.scrollLeft += scrollSpeed * scrollDirection * 1.1;
         let scrollLeft = Math.round(scroller.scrollLeft);
         let maxScrollLeft = Math.round(scroller.scrollWidth - scroller.clientWidth);
         if (-1 === scrollDirection && scrollLeft > 0 || 1 === scrollDirection && scrollLeft < maxScrollLeft) e.preventDefault();
